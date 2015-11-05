dc0b530bebfec8a06b34f44c040ece7abbc2e860
Max Zinkus <mzinkus@calpoly.edu>
2015-11-05 08:31:43 -0800

    Add listcomp quicksort

--- quicksort.hs
+++ quicksort.hs
@@ -1,6 +1,12 @@
 -- O(n log n)
+-- where-clause-filter implementation
 quicksort :: Ord a => [a] -> [a]
 quicksort [] = []
 quicksort (p:xs) = (quicksort lesser) ++ [p] ++ (quicksort greater) -- lazy evaluation O(n)
     where lesser = filter (<= p) xs -- O(n / 2) multiplicative
           greater = filter (> p) xs -- O(n / 2) multiplicative
+
+-- listcomp implementation
+quicksort' :: Ord a => [a] -> [a]
+quicksort' [] = []
+quicksort' (p:xs) = (quicksort' [y | y <- xs, y <= p])  ++ [p] ++ (quicksort' [y | y <- xs, y > p])
