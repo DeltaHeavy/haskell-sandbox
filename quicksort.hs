a1af262451a872e9aef4a4fd77b8f10b48d8f1f6
Max Zinkus <mzinkus@calpoly.edu>
2015-11-05 09:19:41 -0800

    Algos

--- quicksort.hs
+++ quicksort.hs
@@ -1,10 +1,14 @@
+-- Quicksort
 -- O(n log n)
--- where-clause-filter implementation
-quicksort :: Ord a => [a] -> [a]
-quicksort [] = []
+--
+--
+quicksort :: Ord a => [a] -> [a] -- type signature (takes a list of orderables, returns a list of orderables)
+quicksort [] = [] -- case empty list, return empty list
 quicksort (p:xs) = (quicksort lesser) ++ [p] ++ (quicksort greater) -- lazy evaluation O(n)
-    where lesser = filter (<= p) xs -- O(n / 2) multiplicative
-          greater = filter (> p) xs -- O(n / 2) multiplicative
+    where lesser = filter (<= p) xs -- O(n / 2) multiplicative -> log2 n -> O(log n)
+          greater = filter (> p) xs -- O(n / 2) multiplicative -> log2 n -> O(log n)
+--
+-- n * (log n + log n) -> 2(n log n) -> O(n log n)
 
 -- listcomp implementation
 quicksort' :: Ord a => [a] -> [a]
