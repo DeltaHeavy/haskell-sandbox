c5ef6432b151da0bddc7fe57f44d41527ecb8049
Max Zinkus <maxzinkus@gmail.com>
2015-11-12 08:15:45 -0800

    Edit comments

--- quicksort.hs
+++ quicksort.hs
@@ -1,15 +1,14 @@
 -- Quicksort
 -- O(n log n)
 --
---
+-- Readability implementation
 quicksort :: Ord a => [a] -> [a] -- type signature (takes a list of orderables, returns a list of orderables)
 quicksort [] = [] -- case empty list, return empty list
 quicksort (p:xs) = (quicksort lesser) ++ [p] ++ (quicksort greater) -- lazy evaluation O(n)
     where lesser = filter (<= p) xs -- O(n / 2) multiplicative -> log2 n -> O(log n)
           greater = filter (> p) xs -- O(n / 2) multiplicative -> log2 n -> O(log n)
---
--- n * (log n + log n) -> 2(n log n) -> O(n log n)
 
+--
 -- listcomp implementation
 quicksort' :: Ord a => [a] -> [a]
 quicksort' [] = []
