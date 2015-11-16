473082612dff3cc4199d529e156b149257c6954f
Max Zinkus <mzinkus@calpoly.edu>
2015-11-16 14:59:18 -0800

    Edit filter typo

--- filter.hs
+++ filter.hs
@@ -3,4 +3,4 @@ filter' :: (a -> Bool) -> [a] -> [a]
 filter' _ [] = []  
 filter' p (x:xs)   
     | p x       = x : filter p xs  
-    | otherwise = filter p xszith' _ [] = []
+    | otherwise = filter p xs
