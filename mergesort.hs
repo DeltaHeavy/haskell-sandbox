788af65db7acab8efaa5e17e96ac89c9cecba944
Max Zinkus <maxzinkus@gmail.com>
2015-12-13 10:41:40 -0800

    Code cleanup in mergesort

--- mergesort.hs
+++ mergesort.hs
@@ -1,27 +1,27 @@
 -- mergesort
 -- a little more machinery gives us O(N log N) in all cases
--- and it generally more efficient in Haskell's internal implementation
 
--- split takes a list and returns two lists
+-- split takes a list and returns a tuple of lists
 -- it pulls two elements off the front and prepends them to the two return lists
 -- before recursing
-split :: (Ord a) => [a] -> ([a], [a])
+split :: [a] -> ([a], [a])
 split [] = ([], [])
 split [x] = ([x], [])
 split (x:y:zs) = let (xs, ys) = split zs in (x:xs, y:ys)
 
 -- merge takes two lists and returns one
--- this one's easy, use guards to recurse and merge prepending the lower element
+-- this one's easy, use guards to prepend the lower element and then
+-- recurse with the remainder of the list from which we prepended
+-- and the whole list which we didn't touch
 merge :: (Ord a) => [a] -> [a] -> [a]
 merge xs [] = xs
 merge [] ys = ys
-merge (x:xs) (y:ys)
-    | x <= y = x : merge xs (y:ys)
-    | otherwise = y : merge (x:xs) ys
+merge xxs@(x:xs) yys@(y:ys)
+    | x <= y = x : merge xs yys
+    | otherwise = y : merge xxs ys
 
--- mergesort is the driver, taking a list of orderables and returning the sorted list
+-- mergesort takes a list of orderables and returns the sorted list
 mergesort :: (Ord a) => [a] -> [a]
 mergesort [] = []
 mergesort [x] = [x]
-mergesort xs = merge (mergesort xsl) (mergesort xsr)
-    where (xsl, xsr) = split xs
+mergesort xs = let (xsl, xsr) = split xs in merge (mergesort xsl) (mergesort xsr)
