65371bc13f2fea77c6eef4caa4d0e3f64e68026b
Max Zinkus <mzinkus@calpoly.edu>
2016-12-15 01:33:41 -0800

    Start hw3

--- cis194spring13/hw3/hw3.hs
+++ cis194spring13/hw3/hw3.hs
@@ -1,12 +1,23 @@
+-- Max Zinkus
+-- hw3 CIS 194 Spring '13
+--
 
 skips :: [a] -> [[a]]
+
+skip [] _ = []
+skip l n = (head (d l)) : skip (tail l) d
+
+skips s = foldl (:) (zipWith skip s [0..(length s) - 1])
+
+main :: IO ()
+main = do
 -- skips "ABCD" == ["ABCD", "BD", "C", "D"]
 -- skips "hello!"     == ["hello!", "el!", "l!", "l", "o", "!"]
 -- skips [1]          == [[1]]
 -- skips [True,False] == [[True,False], [False]]
 -- skips []           == []
-
-skip _ [] = []
-skip d l = head l : skip d (d (tail l))
-
-skips s = [skip (drop n) (drop n s) | n <- [0..(length s)-1]]
+    putStrLn $ (show (skips "ABCD")) ++ " == " ++ (show ["ABCD", "BD", "C", "D"])
+    putStrLn $ (show (skips "hello!")) ++ " == " ++ (show ["hello!", "el!", "l!", "l", "o", "!"])
+    putStrLn $ (show (skips ([1] :: [Int]))) ++ " == " ++ (show ([[1]] :: [[Int]]))
+    putStrLn $ (show (skips [True,False])) ++ " == " ++ (show [[True, False], [False]])
+    putStrLn $ (show (skips ([] :: [Int]))) ++ " == " ++ (show ([] :: [Int]))
